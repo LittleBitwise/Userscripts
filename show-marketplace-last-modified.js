b897d4075b36bff8b5a8ee3d317b0f46f04e79d6
LittleBitwise <92210138+LittleBitwise@users.noreply.github.com>
2023-05-09 00:50:34 +0300

    Update show-marketplace-last-modified.js
    Added fuzzy dates with contextual fraction.

--- show-marketplace-last-modified.js
+++ show-marketplace-last-modified.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         SL Marketplace Last Modified
+// @name         SL Marketplace Last Modified Date
 // @namespace    https://github.com/LittleBitwise/
 // @version      1.0
 // @description  Displays the last modified date of product images.
@@ -23,7 +23,8 @@
 
 			if (!date) return;
 
-			const lastModifiedElement = createLastModifiedElement(date);
+			const fuzzy = getFuzzyDateFormat(date);
+			const lastModifiedElement = createLastModifiedElement(fuzzy);
 			const productDescription = getProductDescription();
 			productDescription.prepend(lastModifiedElement);
 		}
@@ -53,9 +54,35 @@ function getLastModifiedDateString(response) {
 	return '';
 }
 
+function getFuzzyDateFormat(dateString) {
+	const MS = 1000;
+	const DAY = 86400;
+	const WEEK = 604800;
+	const MONTH = 2592000;
+	const YEAR = 31104000;
+
+	const time = Math.floor((new Date() - new Date(dateString)) / MS)
+
+	// Note: parseFloat() is used convert numbers like "5.0" to "5"
+
+	if (time >= 2 * YEAR) {return `${parseFloat((time/YEAR).toFixed(1))} years ago`;}
+	if (time  > 1 * YEAR) {return `a year ago`;}
+
+	if (time >= 2 * MONTH) {return `${parseFloat((time/MONTH).toFixed(1))} months ago`;}
+	if (time  > 1 * MONTH) {return `a month ago`;}
+
+	if (time >= 2 * WEEK) {return `${parseFloat((time/WEEK).toFixed(1))} weeks ago`;}
+	if (time  > 1 * WEEK) {return `a week ago`;}
+
+	if (time >= 2 * DAY) {return `${parseFloat((time/DAY).toFixed(1))} days ago`;}
+	if (time  > 1 * DAY) {return `yesterday`;}
+
+	return `today`;
+}
+
 function createLastModifiedElement(date) {
 	const strong = document.createElement('strong');
-	strong.innerHTML = 'Last modified: ' + date;
+	strong.innerHTML = 'Date: ' + date;
 
 	const p = document.createElement('p');
 	p.appendChild(strong);
